73b988b72f0fda9bd7090c278ddefd0ef4e084d1
Benoit Hediard <hediard@affinitiz.com>
2012-04-26 23:06:17 +0200

    FacebookRestClient added

---
+++ src/groovy/grails/plugins/facebooksdk/FacebookRestClient.groovy
@@ -0,0 +1,48 @@
+package grails.plugins.facebooksdk
+
+import java.util.Map;
+
+import com.restfb.DefaultLegacyFacebookClient
+import com.restfb.Parameter
+import com.restfb.json.JsonObject
+
+class FacebookRestClient extends DefaultLegacyFacebookClient {
+
+	FacebookRestClient(String accessToken = '') {
+		super(accessToken)
+	}
+	
+	def execute(String method, Map parameters = [:]) {
+		def result = super.execute(method, String, buildVariableArgs(parameters))
+		return parseResult(result)
+	}
+	
+	// PRIVATE
+	
+	private Parameter[] buildVariableArgs(Map parameters) {
+		Parameter[] variableArgs = new Parameter[parameters.size()]
+		parameters.eachWithIndex { key, value, index ->
+			variableArgs[index-1] = Parameter.with(key, value)
+		}
+		return variableArgs
+	}
+	
+	private def parseResult(String result) {
+		if (result.startsWith('{')) {
+			return JSON.parse(result)
+		} else if (result.find('=')) {
+			Map resultMap = [:]
+			result.tokenize('&').each {
+				resultMap[it.tokenize('=')[0]] = it.tokenize('=')[1]
+			}
+			return resultMap
+		} else if (result == 'false') {
+			return false
+		} else if (result == 'true') {
+			return true
+		} else {
+			return result
+		}
+	}
+	
+}
