91822213225286c4663d9521c7e7786b321eb48f
Benoit Hediard <hediard@affinitiz.com>
2012-07-19 10:45:05 +0200

    Plugin cleaning (unused files removed)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -4,22 +4,17 @@ grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-	// inherit Grails' default dependencies
-	inherits("global") {
-		// uncomment to disable ehcache
-		// excludes 'ehcache'
-	}
-	log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+	inherits 'global'
+	log 'warn'
 	repositories {
 		grailsCentral()
 		mavenCentral()
 	}
 	dependencies {
-		compile "com.restfb:restfb:1.6.9"
+		compile 'com.restfb:restfb:1.6.9'
 	}
 	plugins {
-		build(":tomcat:$grailsVersion",
-			  ":release:latest.integration") {
+		build(':release:latest.integration') {
 			export = false
 		}
 	}
