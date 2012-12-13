18577e923a0c0876eca7b4a3ce15deb92691394e
Benoit Hediard <hediard@affinitiz.com>
2012-12-13 16:33:03 +0100

    Resource bug fix

--- grails-app/conf/FacebookSdkResources.groovy
+++ grails-app/conf/FacebookSdkResources.groovy
@@ -1,33 +1,31 @@
 modules = {
-
     'fb-sdk-add-to-page-link' {
         defaultBundle 'facebook-sdk'
-        resource: url: [plugin: 'facebook-sdk', dir: 'js', file: 'add-to-page-link.js']
+        resource url: [plugin: 'facebook-sdk', dir: 'js', file: 'add-to-page-link.js']
     }
 
     'fb-sdk-invite-link' {
         defaultBundle 'facebook-sdk'
-        resource: url: [plugin: 'facebook-sdk', dir: 'js', file: 'invite-link.js']
+        resource url: [plugin: 'facebook-sdk', dir: 'js', file: 'invite-link.js']
     }
 
     'fb-sdk-login-link' {
         defaultBundle 'facebook-sdk'
-        resource: url: [plugin: 'facebook-sdk', dir: 'js', file: 'login-link.js']
+        resource url: [plugin: 'facebook-sdk', dir: 'js', file: 'login-link.js']
     }
 
     'fb-sdk-logout-link' {
         defaultBundle 'facebook-sdk'
-        resource: url: [plugin: 'facebook-sdk', dir: 'js', file: 'logout-link.js']
+        resource url: [plugin: 'facebook-sdk', dir: 'js', file: 'logout-link.js']
     }
 
     'fb-sdk-publish-link' {
         defaultBundle 'facebook-sdk'
-        resource: url: [plugin: 'facebook-sdk', dir: 'js', file: 'publish-link.js']
+        resource url: [plugin: 'facebook-sdk', dir: 'js', file: 'publish-link.js']
     }
 
     'fb-sdk-send-link' {
         defaultBundle 'facebook-sdk'
-        resource: url: [plugin: 'facebook-sdk', dir: 'js', file: 'send-link.js']
+        resource url: [plugin: 'facebook-sdk', dir: 'js', file: 'send-link.js']
     }
-
 }
