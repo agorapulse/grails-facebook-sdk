8e9382f02ba8d35a2be2a3f17a9d83f6c32451fc
Benoit Hediard <ben@agorapulse.com>
2013-05-28 23:17:02 +0200

    Version bumped to 0.4.11

--- FacebookSdkGrailsPlugin.groovy
+++ FacebookSdkGrailsPlugin.groovy
@@ -2,21 +2,21 @@ import grails.plugin.facebooksdk.*
 
 class FacebookSdkGrailsPlugin {
 	
-	def version = "0.4.10"
+	def version = "0.4.11"
 	def grailsVersion = "2.0 > *"
 
     def author = "Benoit Hediard"
-    def authorEmail = "ben@benorama.com"
+    def authorEmail = "ben@agorapulse.com"
     def title = "Facebook SDK Plugin"
     def description = '''The Facebook SDK Plugin allows your Grails application to use the Facebook Platform and develop Facebook apps on Facebook.com or on web sites (with Facebook Connect).
 It is a port of the official Facebook PHP SDK V3.1.1 to Grails 2.0.
 ''' 
     
-    def documentation = "http://benorama.github.io/grails-facebook-sdk/guide/"
+    def documentation = "http://agorapulse.github.io/grails-facebook-sdk/guide/"
     def license = "APACHE"
     def organization = [ name: "AgoraPulse", url: "http://www.agorapulse.com/" ]
-    def issueManagement = [ system: "github", url: "https://github.com/benorama/grails-facebook-sdk/issues" ]
-    def scm = [  url: "https://github.com/benorama/grails-facebook-sdk" ]
+    def issueManagement = [ system: "github", url: "https://github.com/agorapulse/grails-facebook-sdk/issues" ]
+    def scm = [  url: "https://github.com/agorapulse/grails-facebook-sdk" ]
 
     def doWithSpring = {
         // It looks like proxy is automatically generated when request scope bean are injected in grails singleton bean (services)
