6950c8ed4e4666b1edf3ef529af1e58ac6ed0b20
Benoit Hediard <ben@agorapulse.com>
2015-04-24 14:50:56 +0200

    Grails upgraded to 3.0.1

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ repositories {
 }
 dependencies {
     ...
-    compile "org.grails.plugins:facebook-sdk:3.0.0.RC3"
+    compile "org.grails.plugins:facebook-sdk:3.0.1"
 }
 ```
 
--- src/main/groovy/FacebookSdkGrailsPlugin.groovy
+++ src/main/groovy/FacebookSdkGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import org.springframework.aop.scope.ScopedProxyFactoryBean
 class FacebookSdkGrailsPlugin extends Plugin {
 
    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.0.0.RC2 > *"
+    def grailsVersion = "3.0.0 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
