7705739e40ee2aa6a7924149773d3b3d41510ffb
Benoit Hediard <ben@agorapulse.com>
2015-10-30 12:35:37 +0100

    Graph API default to v2.5

--- src/main/groovy/grails/plugin/facebooksdk/FacebookGraphClient.groovy
+++ src/main/groovy/grails/plugin/facebooksdk/FacebookGraphClient.groovy
@@ -13,7 +13,7 @@ import grails.util.Holders
 class FacebookGraphClient extends DefaultFacebookGraphClient {
 
     static final int DEFAULT_READ_TIMEOUT_IN_MS = 180000
-    static final String DEFAULT_API_VERSION = 'v2.4'
+    static final String DEFAULT_API_VERSION = 'v2.5'
 
 	/**
 	 *
@@ -257,15 +257,18 @@ class FacebookGraphClient extends DefaultFacebookGraphClient {
             case 'v2.1':
                 version = Version.VERSION_2_1
                 break
-      			case 'v2.2':
-      				version = Version.VERSION_2_2
-      				break
-      			case 'v2.3':
-      				version = Version.VERSION_2_3
-      				break
+			case 'v2.2':
+				version = Version.VERSION_2_2
+				break
+			case 'v2.3':
+				version = Version.VERSION_2_3
+				break
             case 'v2.4':
-      				version = Version.VERSION_2_4
-      				break
+				version = Version.VERSION_2_4
+				break
+			case 'v2.5':
+				version = Version.VERSION_2_5
+				break
         }
         version
     }
