2ea30f700e0c370ad81c6e34e02bcf0c83d7ec4c
Benoit Hediard <ben@agorapulse.com>
2015-10-30 11:59:24 +0100

    RestFB Java lib upgraded to 1.16.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -28,7 +28,7 @@ grails.project.dependency.resolution = {
         build 'org.apache.httpcomponents:httpclient:4.3.2'
         build 'org.apache.httpcomponents:httpmime:4.3.3'
         // Latest RestFB lib
-		compile 'com.restfb:restfb:1.10.1'
+		compile 'com.restfb:restfb:1.16.0'
 	}
 	plugins {
         build(':release:3.0.1',
